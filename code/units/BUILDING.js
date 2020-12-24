4007fbd950a224a9cf91b3b08e3f5736c533ea0f
Strawberryslol <68839402+justastarwarsstrawberry@users.noreply.github.com>
2020-12-24 11:12:55 +0800

    fixing thingos

--- code/units/BUILDING.js
+++ code/units/BUILDING.js
@@ -354,7 +354,7 @@ exports.code = function(){
 	.setThumbnail('https://cdn.discordapp.com/icons/606586202942079017/7eafb97b0aa80cecb8e4a9f0a7f87c21.webp?size=128')
 	.setImage(('attachment://subturret.png')
 	.setTimestamp()
-	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128'}
+	.setFooter('Edited by: SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128');
 
 
 
