d93d0a6132793eadf9a442f76ce740ee6920a74b
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2021-01-29 13:38:22 -0500

    Remove old useless code

--- code/units - back/!8128316LOADER.js
+++
@@ -1,7 +0,0 @@
-exports.code = function(){
-    const fs = require('fs');
-    var normalizedPath = require("path").join(__dirname, "routes");
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
-  require("./routes/" + file);
-});  
-}
