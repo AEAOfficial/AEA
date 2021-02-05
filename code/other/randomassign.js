3491970634968b618cf1c54865fc16076ff72521
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2021-02-05 11:14:15 -0500

    Update randomassign.js

--- code/other/randomassign.js
+++ code/other/randomassign.js
@@ -35,20 +35,15 @@ client.on("message", message => {
             }
             else {
                 message.delete()
-                function getRandomInt(min, max) {
-                    min = Math.ceil(min);
-                    max = Math.floor(max);
-                    return Math.floor(Math.random() * (max - min)) + min;
-                }
                 channel.send(a1).then(embedMessage => {
-                    var react1 = embedMessage.react('❌')
+                    //var react1 = embedMessage.react('❌')
                     var react2 = embedMessage.react('✅')
-                    if (!react1)
-                        react1.remove()
+                    //if (!react1)
+                        //react1.remove()
                     if (!react2)
                         react2.remove()
-                    if (react1)
-                        var x = getRandomInt(1, 5);
+                    //if (react1)
+                        
 
                     
                 });
@@ -64,15 +59,43 @@ client.on("message", message => {
     }
 
 });
-  
-client.on('messageReactionAdd', (messageReaction, user) => {
-    if (!user.bot || messageReaction.bot)
-    if (messageReaction.emoji.name === '✅')
-    console.log('check')
-    console.log(messageReaction.message.author)
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+    if (user.id === '587367764474462210' || messageReaction.bot) return;
+    const react1 = messageReaction.emoji.name === '✅'
+    const react2 = messageReaction.emoji.name === '❌'
+    if (react1){
+        const { guild } = messageReaction.message
+        const member = guild.members.cache.find(member => member.id === user.id);
+        var x = getRandomInt(1, 3);
+        if (x === 3){
+            member.roles.add('806327965675028520')
+        }
+        if (x === 1){
+            member.roles.add('806328041080750120')
+        }
+        if (x === 2) {
+            member.roles.add('806328050656083998')
+        }
+    }
 })
-  
+client.on('messageReactionRemove', (messageReaction, user) => {
+    if (user.id === '587367764474462210' || messageReaction.bot) return;
+    const react1 = messageReaction.emoji.name === '✅'
+    if (react1){
+        const { guild } = messageReaction.message
+        const member = guild.members.cache.find(member => member.id === user.id);
+        member.roles.remove('806327965675028520')
+        member.roles.remove('806328041080750120')
+        member.roles.remove('806328050656083998')
+    }
+}); 
   
 //________________Dont touch Zone start
 }
