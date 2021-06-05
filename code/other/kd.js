8e66fac234e3f5a509e894120dc6c0a33883cb90
Lemons <lemonsqueezy3601@gmail.com>
2021-06-05 15:32:08 -0400

    Update ranks

--- code/other/kd.js
+++ code/other/kd.js
@@ -11,10 +11,16 @@ exports.code = function(){
         fs.appendFileSync(__dirname + `/db/${username}'s.txt`,`✪`);
     }
           
+    if (message.content.toLowerCase() === '/rank' && message.author.username === 'Vox777'){
+      message.channel.send(`Vox777's Rank: 777`)
+    }
         
-      
+    if (message.content.toLowerCase() === '/rank' && message.author.id === '242687584373964801'){
+      message.channel.send(`Lemons's Rank: ♾`)
+    }
 
     if (message.content.toLowerCase() === '/rank'){
+      if(message.author.username === 'Vox777' || message.author.id === '242687584373964801') return;
       let file = fs.stat(__dirname + `/db/${username}'s.txt`, (err, stats) => {
           console.log('User Read');
           if (stats == undefined) {
