b13c32f81003f84d905c2fdaca732f6178f01878
Lemons <lemonsqueezy3601@gmail.com>
2021-06-05 15:50:51 -0400

    Update ranks

--- code/other/kd.js
+++ code/other/kd.js
@@ -11,20 +11,21 @@ exports.code = function(){
       if(message.author.id === '242687584373964801'){
         message.channel.send(`Lemons's Rank: ♾`)
       }
-      if(message.author.username === 'Vox777') {
+      else if(message.author.username === 'Vox777') {
         message.channel.send(`Vox777's Rank: 777`)
+      } else{
+          let file = fs.stat(__dirname + `/db/${username}'s.txt`, (err, stats) => {
+              console.log('User Read');
+              if (stats == undefined) {
+                message.channel.send(`${username}'s Rank: Unranked`)
+              } else {
+              message.channel.send(`${username}'s Rank: ${stats.size}`)
+              }
+          });
+        }
       }
-      let file = fs.stat(__dirname + `/db/${username}'s.txt`, (err, stats) => {
-          console.log('User Read');
-          if (stats == undefined) {
-            message.channel.send(`${username}'s Rank: Unranked`)
-          } else {
-          message.channel.send(`${username}'s Rank: ${stats.size}`)
+        if(message.content){
+              fs.appendFileSync(__dirname + `/db/${username}'s.txt`,`✪`);
           }
-      });
-    }
-	if(message.content){
-        fs.appendFileSync(__dirname + `/db/${username}'s.txt`,`✪`);
-    }
   });
 }
