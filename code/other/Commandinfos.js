ef610768838cfb485fce8a54abc61d6a315c8b10
Lemons <lemonsqueezy3601@gmail.com>
2021-06-07 13:46:43 -0400

    Added Curly Brace

--- code/other/Commandinfos.js
+++ code/other/Commandinfos.js
@@ -10,19 +10,20 @@ exports.code = function(){
 
 
          const clist = new Discord.MessageEmbed()
-	.setColor('#1500f7')
-	.setTitle('Info Commands')
-	.setURL('')
-	.setAuthor('SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128')
-	.setThumbnail('https://camo.githubusercontent.com/7203f752f0f936475f4ac41dcd3ef9d93af9c2452176a05a1e2b081d1a255ef1/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f69636f6e732f3630363538363230323934323037393031372f34323362346662626563663162626164353566653631343638303337623334642e776562703f73697a653d313238')
+	    .setColor('#1500f7')
+	    .setTitle('Info Commands')
+	    .setURL('')
+	    .setAuthor('SkaarjLord', 'https://cdn.discordapp.com/avatars/287608141191970817/6d82a2d09c9b2323f453abf5bfaaa588.png?size=128')
+	    .setThumbnail('https://camo.githubusercontent.com/7203f752f0f936475f4ac41dcd3ef9d93af9c2452176a05a1e2b081d1a255ef1/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f69636f6e732f3630363538363230323934323037393031372f34323362346662626563663162626164353566653631343638303337623334642e776562703f73697a653d313238')
         .setDescription('command information','\u2800', false)
         .addField ('/AEAElite',false)
         .setDescription('Assigns the AEAElites Role for anyone who wishes it.')
 
-            if(message.content == '/infocommands'){
-	message.channel.send(clist);
-      }
+        if(message.content == '/infocommands'){
+	        message.channel.send(clist);
+        }
    }
+}
 
 //COMMAND INFO
 //AEAElite: Assigns the AEAElites Role for anyone who wishes it.
