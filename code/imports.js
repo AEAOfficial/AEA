547fbfc3bd2f7e926b523ee92eeb6111d516eb13
Strawberryslol <68839402+justastarwarsstrawberry@users.noreply.github.com>
2021-06-08 01:34:52 +0800

    I'm a retard :[

--- code/imports.js
+++ code/imports.js
@@ -10,7 +10,7 @@ client.setMaxListeners(0)
   const teams = require('./other/teams.js');
   const tips = require('./other/tips.js');
   const Timer = require('./other/timer.js');
-  const Info_c = require('./other/infocommands.js');
+  const Info_c = require('./other/Commandinfos.js');
   const Func = require('./other/Votes.js');
   const flip = require('./other/flipcoin.js');
  // const test = require('./other/tournaments.js');
