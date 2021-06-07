4bde0dd43de23b0f9da0cb56955d2bb0d3cce0d0
Lemons <lemonsqueezy3601@gmail.com>
2021-06-07 08:26:04 -0400

    Updated 2v2 maps

--- code/maps/2v2.js
+++ code/maps/2v2.js
@@ -2,20 +2,18 @@ exports.code = function(){
     const { Client, MessageAttachment } = require('discord.js');
     const fs = require("fs");
     client.setMaxListeners(1000)
-
-client.on("message", message => {
-
-function getRandomInt(min, max) {
+    const command = require('./command.js')
+command(client, '2v2', message => {
+    function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
     }
 
-	if(message.content.startsWith('/')){
-        if(message.content.includes('2v2')) {
-        var x = getRandomInt(1, 13);
-        }
-    }
+
+  var x = getRandomInt(1, 6);
+        
+    
   if (x=== 1) {
         const attachment = new MessageAttachment('./resources/map/[p4]Convergence (4p)_map.png');
         message.channel.send(attachment);
@@ -32,54 +30,19 @@ function getRandomInt(min, max) {
           message.reply('Dual Commanders');
     }
   if (x=== 4) {
-        const attachment = new MessageAttachment('./resources/map/[p4]Depth charges (4p) [by hxyy]_map.png');
+        const attachment = new MessageAttachment('./resources/map/[p4]Depth_charges_(4p)_[by_hxyy]_map.png');
         message.channel.send(attachment);
           message.reply('Depth Charges');
     }
   if (x=== 5) {
-        const attachment = new MessageAttachment('./resources/map/[p4]Desert (4p)_map.png');
-        message.channel.send(attachment);
-          message.reply('Desert');
-    }
-  if (x=== 6) {
-        const attachment = new MessageAttachment('./resources/map/[p4]Ice Lake (4p) [by hxyy]_map.png');
-        message.channel.send(attachment);
-          message.reply('Ice Lake');
-    }
-  if (x=== 7) {
-        const attachment = new MessageAttachment('./resources/map/[p4]Island freeze (4p) [by hxyy]_map.png');
-        message.channel.send(attachment);
-          message.reply('Island Freeze');
-    }
-  if (x=== 8) {
-        const attachment = new MessageAttachment('./resources/map/[p4]Islands (4p)_map.png');
-        message.channel.send(attachment);
-          message.reply('Islands');
-    }
-  if (x=== 9) {
-        const attachment = new MessageAttachment('./resources/map/[p4]Lava Maze (4p)_map.png');
-        message.channel.send(attachment);
-          message.reply('Lava Maze');
-    }
-  if (x=== 10) {
-        const attachment = new MessageAttachment('./resources/map/[p4]Lava Vortex (4p)_map.png');
-        message.channel.send(attachment);
-          message.reply('Lava Vortex');
-    }
-  if (x=== 11) {
-        const attachment = new MessageAttachment('./resources/map/[p4]Magma Island (4p)_map.png');
+        const attachment = new MessageAttachment('./resources/map/[p4]Magma_Island_(4p)_map.png');
         message.channel.send(attachment);
           message.reply('Magma Island');
     }
-   if (x=== 12) {
+   if (x=== 6) {
         const attachment = new MessageAttachment('./resources/map/[p4]Glacier (4p)_map.png');
         message.channel.send(attachment);
           message.reply('Glacier');
-    }
-   if (x=== 13) {
-        const attachment = new MessageAttachment('./resources/map/[p4]Nuclear war (4p) [by hxyy]_map.png');
-        message.channel.send(attachment);
-          message.reply('Nuclear War');
     }
 });
 }
