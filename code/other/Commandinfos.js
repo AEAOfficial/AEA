659fa964e7ee180ad2591de989388fe4020d90d0
Strawberryslol <68839402+justastarwarsstrawberry@users.noreply.github.com>
2021-06-08 01:04:18 +0800

    NUKE INCOMING

--- code/other/Commandinfos.js
+++ code/other/Commandinfos.js
@@ -1,1 +1,9 @@
 
+const { Client, MessageAttachment, RichEmbed } = require('discord.js');
+const command = require('./command.js')
+const fs = require('fs')
+exports.code = function(){
+
+    function Timer(){
+        const Timer = setTimeout(timeup, durations[durationType]);
+    }
